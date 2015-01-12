56b4170517601ff4d5220933b86e9dc5529e03a2
timelyportfolio <kent.russell@timelyportfolio.com>
2015-01-12 16:37:40 -0600

    add js lib authors as mentioned in htmlwidgets #55

--- R/svgPanZoom.R
+++ R/svgPanZoom.R
@@ -6,17 +6,44 @@
 #' @import htmlwidgets
 #'
 #' @export
-svgPanZoom <- function(svg, width = NULL, height = NULL) {
+svgPanZoom <- function(svg, ... , width = NULL, height = NULL) {
+
+  # check to see if trellis for lattice or ggplot
+  if(inherits(svg,c("trellis","ggplot"))){
+    # if class is trellis then plot then use grid.export
+    # try to use gridSVG if available
+    if(require("gridSVG")) {
+      show(svg)
+      svg = grid.export(name=NULL)$svg
+    } else {  #use SVGAnnotation
+      if(require(SVGAnnotation)){
+        warning("for best results with ggplot2 and lattice, please install gridSVG")
+        svg = svgPlot(svg, addInfo = F)
+      } else { # if
+        stop("SVGAnnotation or gridSVG required with lattice or trellis objects")
+      }
+    }
+  }
 
   # check to see if svg is XML and saveXML if so
-  if(inherits(s,"XMLAbstractDocument")){
+  if(inherits(svg,c("XMLAbstractDocument","XMLAbstractNode"))){
     # should we add check for svg element?
-    svg = XML::saveXML(s)
+    svg = XML::saveXML(svg)
   }
+
+  # use SVG file if provided
+  # thanks @jjallaire for code from https://github.com/rich-iannone/DiagrammeR
+  # to use file or connection
+  if ( inherits(svg, "connection") || ( class(svg) == "character" && file.exists(svg) ) ){
+    # might want to parse to insure validity
+    svg <- readLines(diagram, warn = FALSE)
+  }
+
 
   # forward options using x
   x = list(
     svg = svg
+    ,config = list(...)
   )
 
   # create widget
--- inst/htmlwidgets/svgPanZoom.js
+++ inst/htmlwidgets/svgPanZoom.js
@@ -13,12 +13,18 @@ HTMLWidgets.widget({
   },
 
   renderValue: function(el, x, instance) {
-
+    // set innerHTML equal to the SVG provided by R as x.svg
+    //  for better robustness, create DOM element separately
+    //  and add to the htmlwidgets container el
     el.innerHTML = x.svg;
 
     var svg = el.getElementsByTagName("svg")[0]
 
-    instance = svgPanZoom(svg);
+    instance.zoomWidget = svgPanZoom(svg, x.config);
+    //  use expando property so we can access later
+    //    somewhere saw where expando can cause memory leak in IE
+    //    could also set in HTMLWidgets.widgets[x] where matches el
+    el.zoomWidget = instance.zoomWidgets;
 
   },
 
