fd5919eb096fca35488f4c40c9733dff168b107c
timelyportfolio <kent.russell@timelyportfolio.com>
2015-08-20 06:13:47 -0500

    add touch support with hammer.js

--- inst/htmlwidgets/svgPanZoom.js
+++ inst/htmlwidgets/svgPanZoom.js
@@ -35,6 +35,56 @@ HTMLWidgets.widget({
 
     makeResponsive(el);
 
+    // add touch with hammer.js
+    //  using code from example
+    //  https://github.com/ariutta/svg-pan-zoom/blob/master/demo/mobile.html
+    x.config.customEventsHandler = {
+      haltEventListeners: ['touchstart', 'touchend', 'touchmove', 'touchleave', 'touchcancel']
+    , init: function(options) {
+        var instance = options.instance
+          , initialScale = 1
+          , pannedX = 0
+          , pannedY = 0
+        // Init Hammer
+        // Listen only for pointer and touch events
+        this.hammer = Hammer(options.svgElement, {
+          inputClass: Hammer.SUPPORT_POINTER_EVENTS ? Hammer.PointerEventInput : Hammer.TouchInput
+        })
+        // Enable pinch
+        this.hammer.get('pinch').set({enable: true})
+        // Handle double tap
+        this.hammer.on('doubletap', function(ev){
+          instance.zoomIn()
+        })
+        // Handle pan
+        this.hammer.on('panstart panmove', function(ev){
+          // On pan start reset panned variables
+          if (ev.type === 'panstart') {
+            pannedX = 0
+            pannedY = 0
+          }
+          // Pan only the difference
+          instance.panBy({x: ev.deltaX - pannedX, y: ev.deltaY - pannedY})
+          pannedX = ev.deltaX
+          pannedY = ev.deltaY
+        })
+        // Handle pinch
+        this.hammer.on('pinchstart pinchmove', function(ev){
+          // On pinch start remember initial zoom
+          if (ev.type === 'pinchstart') {
+            initialScale = instance.getZoom()
+            instance.zoom(initialScale * ev.scale)
+          }
+          instance.zoom(initialScale * ev.scale)
+        })
+        // Prevent moving the page on some devices when panning over SVG
+        options.svgElement.addEventListener('touchmove', function(e){ e.preventDefault(); });
+      }
+    , destroy: function(){
+        this.hammer.destroy()
+      }
+    }
+
     instance.zoomWidget = svgPanZoom(svg, x.config);
 
     // add back viewBox that svgPanZoom removes to fill the container
